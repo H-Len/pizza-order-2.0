5e8f61a741a15942ae9ce743888704b435187ef5
Hannah Melendy <hrmelendy@gmail.com>
2019-06-23 14:41:14 -0700

    allow user to order pizza size and toppings, and see total on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,4 @@
 //business logic
-//
-// function Pizza(sizes, toppings, totalPrice, orderName) {
-//   this.sizes = ["sm", "md", "lg"];
-//   // this.toppings = ["fig", "olive", "onion"];
-//   this.totalPrice = parseInt(10);
-//   this.orderName = "";
-// }
-
-//add price of size of pizza to total cost
-// Pizza.prototype.sizeCost = function() {
-//   var sizeTotal = this.totalPrice;
-//   sizeTotal += parseInt(yourSize);
-//   console.log(sizeTotal);
-//   return sizeTotal;
-// }
 
 //constructor for bill (all due)
 function Bill(sizes, totalPrice, orderName, toppingCost, tab){
@@ -21,21 +6,17 @@ function Bill(sizes, totalPrice, orderName, toppingCost, tab){
   // this.toppings = ["fig", "olive", "onion"];
   this.totalPrice = parseInt(10);
   this.orderName = "";
-  // this.doughCost = 10,
-  // this.sizeCost = sizeCost,
+
   this.toppingCost = 0,
   this.tab = tab
 }
 
 Bill.prototype.tabCost = function() {
+  //add price depending on size
   var sizeTotal = this.totalPrice;
   sizeTotal += parseInt(yourSize);
-  console.log(sizeTotal);
-//   return sizeTotal;
-// }
-//
-// //add all toppings that are checked by user
-// Bill.prototype.onTop = function () {
+
+  //add all toppings that are checked by user
   var toppingTotal = this.toppingCost;
   if(document.getElementById("fig").checked) {
     toppingTotal ++;
@@ -46,26 +27,21 @@ Bill.prototype.tabCost = function() {
   if(document.getElementById("onion").checked) {
     toppingTotal++;
   }
-    console.log(toppingTotal);
-//};
 
-//REFACTOR
-// Bill.prototype.sumCost = function(tab) {
-
+  //calculate total due
   this.tab = sizeTotal + toppingTotal;
-  console.log(this.tab);
+  totalDue = this.tab;
+  return totalDue;
 };
+
+
 
 var orderName = "";
 var yourBill = new Bill(yourSize);
 var yourSize = yourBill.size;
-// var yourBill = new Bill()
-// var sizeP = "";
 var sizeTotal = "";
-var totalDue;
-var tab;
-// var perTop = "";
-// var totalDue = yourSize + perTop;
+var totalDue = "";
+var tabCost;
 
 
 
@@ -74,36 +50,32 @@ var tab;
 //UI logic
 $(function() {
 
-  //get name for order
-    $("#nameBtn").click(function(event) {
-      event.preventDefault();
-      orderName = $("#userName").val();
-      $(".jumbotron").show();
-      $(".greet").html(orderName);
-    });
+//get name for order
+  $("#nameBtn").click(function(event) {
+    event.preventDefault();
+    orderName = $("#userName").val();
+    $(".jumbotron").show();
+    $(".greet").html(orderName);
+  });
 
-    $("#yourOrder").submit(function(event) {
-      event.preventDefault();
-      yourSize = $(".sizes").val();
-      $(".sizeVal").text(yourSize);
-      // $(".topper").val();
+  $("#yourOrder").submit(function(event) {
+    event.preventDefault();
+    yourSize = $(".sizes").val();
+    $(".sizeVal").text(yourSize);
 
-      totalDue = $(yourBill.tabCost());
-      // console.log(sizeP);
-      // perTop = $(yourBill.onTop());
-      // tab = yourPizza.sumCost;
-      // console.log(tab);
-      $(".grandOrder").show(function() {
-        setInterval(function() {
-          window.scrollTo(0,document.body.scrollHeight);
-        });
-        // // $("#totalCost").text(yourBill.sumCost());    //it says "sizeTotal" is returned as [object Object], but it's returned from .sizeCost() and other steps are taken to set it
-
-      })
-    })
-
-      $(".refresh").show()
-      $(".refresh").click(function() {
-        window.location.reload();
+    totalDue = $(yourBill.tabCost());
+    $(".grandOrder").show(function(totalDue) {
+      setInterval(function() {
+        window.scrollTo(0,document.body.scrollHeight);
       });
+
+      //display total at bottom of page
+      $("#totalCost").text(yourBill.tabCost());
+    })
+  })
+
+  $(".refresh").show()
+  $(".refresh").click(function() {
+    window.location.reload();
+  });
 });
